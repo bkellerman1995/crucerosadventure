231a31d17814d04afae17fb265ed93f66089f606
bkellerman1995 <bkellerman@microsoft.com>
2025-02-25 17:53:12 -0600

    Se cambió el código de la línea 163 de ListHabitaciones.jsx de
    " return navigate(`/movie/update/${idSelected}`);"  a
    " return navigate(`/habitacion/update/${idSelected}`);"

--- src/components/Layout/ListBarcos.jsx
+++ src/components/Layout/ListBarcos.jsx
@@ -85,25 +85,22 @@ function TableHabitacionesHead(props) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell>
-
-        </TableCell>
+        <TableCell></TableCell>
         {headCells.map((headCell) => (
-
           <TableCell
             key={headCell.id}
-            padding={headCell.disablePadding ? 'none' : 'normal'}
+            padding={headCell.disablePadding ? "none" : "normal"}
             sortDirection={orderBy === headCell.id ? order : false}
           >
             <TableSortLabel
               active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
+              direction={orderBy === headCell.id ? order : "asc"}
               onClick={createSortHandler(headCell.id)}
             >
               {headCell.label}
               {orderBy === headCell.id ? (
                 <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
                 </Box>
               ) : null}
             </TableSortLabel>
@@ -129,7 +126,7 @@ function TableHabitacionesToolbar(props) {
   const { idSelected } = props;
   const navigate = useNavigate();
   const update = () => {
-    return navigate(`/movie/update/${idSelected}`);
+    return navigate(`/barco/update/${idSelected}`);
   };
   return (
     <Toolbar
