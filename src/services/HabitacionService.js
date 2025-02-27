f378382dab4ece6941c7b9ee10dd05e9e1aefd03
118320754 <alrodriguezal@est.utn.ac.cr>
2025-02-27 17:25:46 -0600

    añadir detail habitacion, cambios habitacionService y main

--- src/services/HabitacionService.js
+++ src/services/HabitacionService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //Esta declaracion lanza undefined
-//const BASE_URL = import.meta.env.VITE_BASE_URL + 'habitacion';
+// const BASE_URL = import.meta.env.VITE_BASE_URL + 'habitacion';
 //console.log ('BASE_URL: ', BASE_URL);
 
 
@@ -10,9 +10,28 @@ class HabitacionService {
     
     //Lista de todas las habitaciones.
     //localhost:81/crucerosadventure/habitacion
-    getHabitaciones() {
+    getHabitacion() {
         return axios.get('http://localhost:81/crucerosadventure/habitacion');
     }
+
+    //Obtener pelicula
+    //localhost:81/crucerosadventure/habitacion/1
+    getHabitacionById(HabitacionesId){
+        return axios.get('http://localhost:81/crucerosadventure/habitacion'+'/'+HabitacionesId);
+    }
+
+    // createHabitacion(Habitacion) {
+    //     return axios.post(BASE_URL, JSON.stringify(Habitacion));
+    //   }
+      
+    //   updateHabitacion(Habitacion) {
+    //     return axios({
+    //       method: 'put',
+    //       url: BASE_URL,
+    //       data: JSON.stringify(Habitacion)
+    
+    //     })
+    //   }
 }
 
 export default new HabitacionService()
