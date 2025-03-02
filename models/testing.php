edc552c0692b78c9d2b52ac7333d0342036ae03d
dan2510 <carballo2597@gmail.com>
2025-03-02 15:36:29 -0600

    test

---
+++ models/testing.php
@@ -0,0 +1,55 @@
+<?php
+class BarcoModel
+{
+    //Conectarse a la BD
+    public $enlace;
+
+    public function __construct()
+    {
+        $this->enlace = new MySqlConnect();
+    }
+    /**
+     * Listar barcos
+     * @param 
+     * @return $vResultado - Lista de objetos
+     */
+    public function all()
+    {
+        try {
+            //Consulta SQL
+            $vSQL = "SELECT b.descripcion, b.capacidadHuesped,COUNT(h.idHabitacion) AS canHabitaciones
+            FROM barco b LEFT JOIN habitacion h ON b.idbarco = h.idbarco GROUP BY b.idbarco
+            order by b.idbarco desc;";
+            //Ejecutar la consulta
+            $vResultado = $this->enlace->ExecuteSQL($vSQL);
+            //Retornar la respuesta
+
+            return $vResultado;
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+
+        /*Obtener */
+        public function get($id)
+        {
+            try {
+                //Consulta sql               
+                $vSQL = "SELECT * FROM barco where idbarco=$id";
+
+                // Ejecutar la consulta
+                $vResultado = $this->enlace->ExecuteSQL($vSQL);
+
+                if (!empty($vResultado)) {
+                    // Retornar el objeto
+                    return $vResultado[0];
+                    
+                }
+                return $vResultado;
+
+            } catch (Exception $e) {
+                handleException($e);
+            }
+        }
+ 
+}
