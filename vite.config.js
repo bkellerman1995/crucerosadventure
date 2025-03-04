387182cbcb3bcf12545eb2afab6b0589ee3a0e5e
dan2510 <carballo2597@gmail.com>
2025-03-03 22:22:34 -0600

    tratando de importar boostrap

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import jsconfigPaths from "vite-jsconfig-paths"
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 // https://vite.dev/config/
 export default defineConfig({
