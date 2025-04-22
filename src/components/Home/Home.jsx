0e189706bff4feb523841e6887b5a193fc96577c
bkellerman1995 <bkellerman@microsoft.com>
2025-04-22 11:45:04 -0600

    Se agrego un video de fondo en la página de inicio

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,20 +3,48 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 export function Home() {
   return (
-    <Container sx={{ p: 2 }} maxWidth="sm">
+    <Container
+      maxWidth="100%"
+      style={{ position: "relative", padding:0, margin: 0 }}
+    >
+      {/* Video de fondo o en la parte superior */}
+      <div
+        style={{
+          position: "absolute",
+          width: "105%",
+          marginLeft: -30,
+          height: "100vh", // Hace que el video cubra toda la altura de la pantalla
+          top: 0,
+          left: 0,
+          zIndex: -1, // Para poner el video en el fondo
+        }}
+      >
+        <video
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+          autoPlay
+          loop
+          muted
+        >
+          <source src="../uploads/videoCrucero3.mp4" type="video/mp4" />
+          Tu navegador no soporta el formato de video.
+        </video>
+      </div>
       <Typography
         component="h1"
         variant="h2"
         align="center"
-        color="text.primary"
         gutterBottom
+        style={{ color: "white" }}
       >
         CRuceros Adventure
       </Typography>
-      <Typography variant="h5" align="center" color="text.secondary">
+      <Typography style={{ color: "white" }} variant="h5" align="center">
         Reserva un viaje en crucero.
       </Typography>
-      <img src="../uploads/LogoCrucerosAdventure.png" />
     </Container>
   );
 }
